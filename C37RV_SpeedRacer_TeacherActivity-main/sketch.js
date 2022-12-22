9ed7013f5294480085aca04e02329f285d682961
Coolboy145 <88191598+Coolboy145@users.noreply.github.com>
2022-12-22 18:02:57 +0000

    Add files via upload

---
+++ C37RV_SpeedRacer_TeacherActivity-main/sketch.js
@@ -0,0 +1,31 @@
+var canvas;
+var backgroundImage;
+var database, gameState;
+var form, player, playerCount;
+var allPlayers;
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if(playerCount===2){
+    game.update(1)
+  }
+  if(gameState===1){
+    game.play()
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
